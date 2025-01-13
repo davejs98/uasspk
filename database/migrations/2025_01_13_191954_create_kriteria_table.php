349fb005ca94066a502d1d2af117493233e85533
David <dave.js980@gmail.com>
2025-01-14 02:47:23 +0700

    kriteriaSeeder

---
+++ database/migrations/2025_01_13_191954_create_kriteria_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKriteriaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kriteria', function (Blueprint $table) {
+            $table->string('idKriteria', 10)->primary(); // Primary key
+            $table->string('namaKriteria'); // Nama kriteria
+            $table->enum('tipe', ['Benefit', 'Cost']); // Tipe (Benefit/Cost)
+            $table->string('bobot'); // Bobot (e.g., "15%")
+            $table->timestamps(); // created_at and updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kriteria');
+    }
+}
---
+++ database/seeders/KriteriaSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class KriteriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('Kriteria')->insert([
+            'idKriteria' => 'K1',
+            'namaKriteria' => 'Riwayat Pendidikan',
+            'tipe' => 'Benefit',
+            'bobot' => '15%',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
