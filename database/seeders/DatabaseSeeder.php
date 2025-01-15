26710c2a6dad76f527647c2ee2527e9301d3f8f9
David <dave.js980@gmail.com>
2025-01-15 13:11:36 +0700

    new

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
         $this->call(KriteriaSeeder::class);
+        $this->call(CalonKaryawanSeeder::class);
+        ;
     }
 }
